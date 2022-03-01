0410cf560a5f35b741167e61b3861f264b469307
jia <13520723393@163.com>
2022-03-01 19:46:18 +0800

    save

--- FFmpeg.cpp
+++ FFmpeg.cpp
@@ -41,39 +41,62 @@ void FFmpeg::record_video() {
     av_dict_set(&options, "video_size", "640x360", 0);
     av_dict_set(&options, "pixel_format", "yuv420p", 0);
     av_dict_set(&options, "framerate", "30", 0);
-    AVFormatContext *ctx = nullptr;
-    auto res = avformat_open_input(&ctx, DEVICE, fmt, &options);
+    AVFormatContext *input_format_ctx = nullptr;
+    auto res = avformat_open_input(&input_format_ctx, DEVICE, fmt, &options);
     if (res < 0) {
         av_log(nullptr, AV_LOG_ERROR, "%s", av_err2str(res));
         return;
     }
-    AVCodecContext *codecContext = nullptr;
-
-//    auto codec = avcodec_find_encoder_by_name("x264");
-//    res = avcodec_open2(codecContext, codec, nullptr);
-//    if (res < 0) {
-////        av_log(nullptr, AV_LOG_ERROR, "%s", av_err2str(res));
-////        return;
-//    }
-    auto params = ctx->streams[0]->codecpar;
+    AVFormatContext *output_format_ctx = nullptr;
+    avformat_alloc_output_context2(&output_format_ctx, nullptr, "h264", "cam.h264");
+    avformat_init_output(output_format_ctx, nullptr);
+    auto output_codec = avcodec_find_encoder(AV_CODEC_ID_H264);
+    auto output_codec_ctx = avcodec_alloc_context3(output_codec);
+    auto output_stream = avformat_new_stream(output_format_ctx, output_codec);
+    avcodec_parameters_copy(output_stream->codecpar,input_format_ctx->streams[0]->codecpar);
+    avformat_write_header(output_format_ctx, nullptr);
+    if (res < 0) {
+        av_log(nullptr, AV_LOG_ERROR, "%s", av_err2str(res));
+        return;
+    }
+    auto params = input_format_ctx->streams[0]->codecpar;
     auto bufferSize = av_image_get_buffer_size((AVPixelFormat) (params->format), params->width, params->height, 1);
-    auto passthroughDecoder = avcodec_find_decoder(AV_CODEC_ID_WRAPPED_AVFRAME);
-    auto passthroughDecoderCtx = avcodec_alloc_context3(passthroughDecoder);
-    std::ofstream file("./record_video.yuv");
     auto pkt = av_packet_alloc();
     auto frame = av_frame_alloc();
     auto count = 60;
     while (true) {
-        auto ret = av_read_frame(ctx, pkt);
+        auto ret = av_read_frame(input_format_ctx, pkt);
         if (ret == 0) {
             unwrap_packet(pkt, frame);
-            res = avcodec_send_frame(codecContext, frame);
-            avcodec_receive_packet(codecContext, pkt);
+            res = avcodec_send_frame(output_codec_ctx, frame);
+            if (res == AVERROR(EAGAIN)) {
+                av_log(nullptr, AV_LOG_INFO, "send frame continue");
+                continue;
+            }
             if (res < 0) {
                 av_log(nullptr, AV_LOG_ERROR, "%s", av_err2str(res));
                 return;
             }
-            av_packet_unref(pkt);
+            while (true) {
+                res = avcodec_receive_packet(output_codec_ctx, pkt);
+                if (res == AVERROR(EAGAIN)) {
+                    break;
+                }
+                if (res < 0) {
+                    av_log(nullptr, AV_LOG_ERROR, "%s", av_err2str(res));
+                    return;
+                }
+                pkt->stream_index = 0;
+                res = av_interleaved_write_frame(output_format_ctx, pkt);
+                if (res < 0) {
+                    av_log(nullptr, AV_LOG_ERROR, "%s", av_err2str(res));
+                    return;
+                }
+            }
+            if (res < 0) {
+                av_log(nullptr, AV_LOG_ERROR, "%s", av_err2str(res));
+                return;
+            }
         } else if (ret == AVERROR(EAGAIN)) {
             continue;
         } else {
@@ -82,11 +105,10 @@ void FFmpeg::record_video() {
         if (count-- <= 0) {
             break;
         }
-
     }
     av_packet_free(&pkt);
-    file.close();
-    avformat_close_input(&ctx);
+    avformat_close_input(&input_format_ctx);
+    av_write_trailer(output_format_ctx);
 }
 
 
